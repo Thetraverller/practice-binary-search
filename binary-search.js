29ba177c9b7c9138643abc67d6bf6e987a9f599b
Tanzil <mtanzilislam1997@gmail.com>
2022-12-01 18:43:38 +0000

    fingit add .git add .git add .git add .git add .!

--- binary-search.js
+++ binary-search.js
@@ -1,19 +1,34 @@
 function linearSearch (arr, target) {
 
   // Can you solve this in one line?
-
+    return arr.indexOf(target)
 };
+
+
 
 function binarySearch(arr, target) {
 
   // Set integers pointing to the high and low range of possible indices
+  let low = 0;
+  let high = arr.length -1;
 
   // While high and low indices do not overlap...
+  while(high >= low){
 
     // Find the midpoint between high and low indices
+      let mp = Math.floor((high + low) / 2);
 
     // Compare the target value to the midpoint value
+       if(target === arr[mp]) {
+        return mp
+       } else if (target > arr[mp]) {
+        low = mp + 1
+       } else if (target < arr[mp]) {
+        high = mp - 1
+      }
+    }
 
+    return -1
     // If the target equals the midpoint...
       // Return the midpoint index
     // If the target is higher than the midpoint...
@@ -24,6 +39,9 @@ function binarySearch(arr, target) {
   // Return -1 if the loop exits with overlapping pointers
 
 }
+
+console.log(binarySearch([2,4,6,8], 6));  // 2
+console.log(binarySearch([2,4,6,8], 10));  // -1
 
 
 module.exports = [linearSearch, binarySearch]
